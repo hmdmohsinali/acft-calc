7b20cd174a6f2a6eb05faf1ca13132f71e3c338c
Mohsin A. <hmdmohsinali@gmail.com>
2024-08-07 11:35:27 +0500

    completed

---
+++ acft-calc/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ACFTCalculator from './components/ACFTCalc.jsx'
+import AcftCalcc from "./components/AcftCalcc.jsx";
+function App() {
+
+  return (
+    <>
+  
+    <AcftCalcc/>
+     </>
+  )
+}
+
+export default App
