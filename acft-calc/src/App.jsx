f3bea5d1f63e022ef4cddb8acb4f4e95be824b27
Mohsin A. <hmdmohsinali@gmail.com>
2024-08-09 11:08:01 +0500

    check now

--- acft-calc/src/App.jsx
+++ acft-calc/src/App.jsx
@@ -1,7 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import ACFTCalculator from './components/ACFTCalc.jsx'
 import AcftCalcc from "./components/AcftCalcc.jsx";
 function App() {
 
